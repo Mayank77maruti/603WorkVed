2cffab844a440f766c060353a44297889299c1c8
Eshita Talukdar <eshitatalukdar0@gmail.com>
2025-01-09 23:16:18 -0800

    Moved FAQ styles and scripts to centralized CSS and JS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -233,3 +233,24 @@ const handleScroll = () => {
 
 window.addEventListener('scroll', handleScroll);
 handleScroll();
+
+/**
+ * FAQ SECTION
+ */
+
+const faqQuestions = document.querySelectorAll('.faq-question');
+faqQuestions.forEach(question => {
+    question.addEventListener('click', () => {
+        const answer = question.nextElementSibling;
+        if (answer.style.display === 'block') {
+            answer.style.display = 'none';
+        } else {
+            answer.style.display = 'block';
+        }
+        faqQuestions.forEach(q => {
+            if (q !== question) {
+                q.nextElementSibling.style.display = 'none';
+            }
+        });
+    });
+});
